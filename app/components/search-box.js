d044226c9d67afe0a63949afbae8cdc7f62a047d
Sammy Nave <sammy.nave@meyouhealth.com>
2015-06-24 17:04:30 -0400

    add some setup

---
+++ app/components/search-box.js
@@ -0,0 +1,3 @@
+import SearchBox from 'ember-cli-dc-search/components/search-box';
+
+export default SearchBox;
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-dc-search'
+  name: 'ember-cli-dc-search',
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/search-button/dist/js/search-button.js');
+    app.import(app.bowerDirectory + '/search-button/dist/css/search-button.css');
+  }
 };
